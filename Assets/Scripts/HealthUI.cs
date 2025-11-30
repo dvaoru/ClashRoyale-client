9665d77e6817a11d8747edbdda1fd54ea7a56c30
Orlov <dvaoru@gmail.com>
2025-11-30 23:35:32 +0300

    Правка орфрграфии

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 public class HealthUI : MonoBehaviour
 {
     [SerializeField] private RectTransform _filledImage;
-    [SerializeField] private float _defoaltWidth = 0f;
+    [SerializeField] private float _defoltWidth = 0f;
     [SerializeField] private Canvas _canvas;
     [SerializeField] private TextMeshProUGUI _healthText;
 
-    private Camera mainCamera;
+    private Camera _mainCamera;
     private void Awake()
     {
-        mainCamera = Camera.main;
-        _defoaltWidth = _filledImage.sizeDelta.x;
+        _mainCamera = Camera.main;
+        _defoltWidth = _filledImage.sizeDelta.x;
     }
 
     public void UpdateHealth(float max, float current)
     {
         float percent = current / max;
-        _filledImage.sizeDelta = new Vector2(_defoaltWidth * percent, _filledImage.sizeDelta.y);
+        _filledImage.sizeDelta = new Vector2(_defoltWidth * percent, _filledImage.sizeDelta.y);
         _healthText.text = current + " / " + max;
     }
 
 
     private void LateUpdate()
     {
-        if (_canvas == null || mainCamera == null) return;
-        Vector3 dir = _canvas.transform.position - mainCamera.transform.position;
+        if (_canvas == null || _mainCamera == null) return;
+        Vector3 dir = _canvas.transform.position - _mainCamera.transform.position;
         dir.x = 0f;
         if (dir.sqrMagnitude < 0.001f) return;
         _canvas.transform.rotation = Quaternion.LookRotation(dir);
